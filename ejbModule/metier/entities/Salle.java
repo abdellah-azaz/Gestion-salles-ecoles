2a951b1057c398f58cafb529f5f6bf6dfd7f6eee
ELGUAILKAMAL <elguailkamal@gmail.com>
2024-11-23 19:51:30 +0100

    TEST

--- ejbModule/metier/entities/Salle.java
+++ ejbModule/metier/entities/Salle.java
@@ -7,7 +7,9 @@ import javax.persistence.*;
 @Table(name = "Salle")
 public class Salle implements Serializable {
 
-    @Id
+	private static final long serialVersionUID = 1L;
+
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id; // Clé primaire pour l'entité Charge
 
